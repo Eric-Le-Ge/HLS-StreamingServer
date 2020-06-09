4bf65a99314b0d8ac7682b6bb4645cea312debf8
Eric Ge <ericlege@berkeley.edu>
2020-06-09 13:56:30 -0700

    update install_file.sh

--- scripts/install_files.sh
+++ scripts/install_files.sh
@@ -4,6 +4,9 @@ mkdir /nginx/hls
 chown -R www-data:www-data /nginx
 # move video player to path
 cp assets/index.html /nginx/index.html
+# place server ip address in video player
+IP=$(curl https://ifconfig.me)
+sed -i 's/YOUR_SERVER_IP/$(IP)/g' /nginx/index.html
 # replace nginx conf with new conf
 mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.old
 cp assets/nginx.conf /usr/local/nginx/conf/nginx.conf
