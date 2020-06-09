b8309c016891e1b68ad8e1db2c32c46c868bf331
Eric Ge <ericlege@berkeley.edu>
2020-06-09 12:48:39 -0700

    install files

--- scripts/install_files.sh
+++ scripts/install_files.sh
@@ -3,7 +3,7 @@ mkdir /nginx/hls
 # make path /nginx accessible to client
 chown -R www-data:www-data /nginx
 # move video player to path
-mv assets/index.html /nginx/index.html
+cp assets/index.html /nginx/index.html
 # replace nginx conf with new conf
 mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.old
-mv assets nginx.conf /usr/local/nginx/conf/nginx.conf
+cp assets/nginx.conf /usr/local/nginx/conf/nginx.conf
